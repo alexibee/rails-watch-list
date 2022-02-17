ecfb42651516cfdc63656d94ec8ce87d9d2cbe02
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-17 20:29:44 +0000

    another try!

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,5 +4,4 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
-  validates :image_url, format: { with: /\A(http:|https:).{6}(\.jpeg|\.png|\.jpg)/, message: 'Input a valid URL' }
 end
