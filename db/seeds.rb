44d21d57a8157a4a3e848ded37cbf3720c26de46
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-02-17 13:02:55 +0000

    routes create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -14,6 +16,7 @@ Movie.destroy_all
 doc = URI.open(url).read
 parsed = JSON.parse(doc)
 
-parsed["results"].each do |movie|
-  Movie.create!(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}", rating: movie["vote_average"])
+parsed['results'].each do |movie|
+  Movie.create!(title: movie['title'], overview: movie['overview'],
+                poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}", rating: movie['vote_average'])
 end
